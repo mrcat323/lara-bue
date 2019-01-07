2b16de1c1d10f03de7bc511ad462795ab13454c8
Mr. CaT <mrcat323@gmail.com>
2019-01-07 21:02:25 +0500

    Access-Point view added

---
+++ src/views/init.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    
+    <title>Lara-Bue - Quick Starter - Boost your development</title>
+    
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    
+</head>
+<body>
+    <div id="app">
+        <app></app>
+    </div>
+</body>
+</html>
